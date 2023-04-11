5e3194d36bf9e8dc0ce7c1175a51169777f8c3c7
Velimir <87824619+VtheSaint@users.noreply.github.com>
2023-04-11 14:56:45 +0300

    initial

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::net::SocketAddr;
+use axum::{response::Html, routing::get, Router};
+use dotenv::dotenv;
+use std::env;
+
+mod adapters;
+mod connector;
+// mod routes;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let app: Router = Router::new().route("/", get(root));
+
+    let parsed_address= env::var("SERVER_ADDRESS")
+        .unwrap_or_else(|e| panic!("Failed to get env with name 'SERVER_ADDRESS': {:?}", e));
+
+    // TODO: to future
+    let database_url = env::var("DATABASE_URL")
+        .unwrap_or_else(|e| panic!("Failed to get env with name 'DATABASE_URL': {:?}", e));
+
+    let addr: SocketAddr = SocketAddr::from(parsed_address);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> Html<&'static str> {
+    Html("hello world")
+}
